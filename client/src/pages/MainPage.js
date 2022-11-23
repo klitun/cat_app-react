7478626ca4c4dc0acaebd5eef68800f6335a1126
klitun <klitun@yahoo.com>
2022-11-22 17:45:57 -0800

    trying to structure the main page

---
+++ client/src/pages/MainPage.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import Card from '../components/Card'
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+
+function Home() {
+    return (
+      <>
+        <Header />
+        <Navbar />
+        <Card />
+        <Card />
+        <Card />
+        <Footer />
+      </>
+    );
+  }
+  
+  export default Home;
+  
