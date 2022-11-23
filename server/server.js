67bca99caceba62053087fb5b50d944be30e786a
kaihuan Huang <huanhuan@kaihuans-MacBook-Pro.local>
2022-11-22 18:23:36 -0800

    update with Schemas files

---
+++ server/server.js
@@ -0,0 +1,38 @@
+//Implement the Apollo Server and apply it to the Express server as middleware.
+const { ApolloServer } = require("apollo-server-express");
+const { typeDefs, resolvers } = require("./Schema");
+// Update the auth middleware function to work with the GraphQL API.
+const { authMiddleware } = require('./utils/auth');
+const express = require('express');
+const path = require('path');
+const db = require('./config/connection');
+const routes = require('./routes');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+server.applyMiddleware({ app });
+
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+// app.use(routes);
+
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
+  });
+});
